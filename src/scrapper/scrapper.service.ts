6c6b0020fb6d7f756cf6e789d7afe645bb2cf07f
Franco Cabrera <cabrera.franco@outlook.com>
2024-01-09 16:22:08 -0300

    post install

--- src/scrapper/scrapper.service.ts
+++ src/scrapper/scrapper.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import * as puppeteer from 'puppeteer';
+// import * as puppeteer from 'puppeteer-core';
+// import chrome from 'chrome-aws-lambda';
 import { ScrapperData } from './interface/scrapper.interface';
 import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class ScrapperService {
+  private chrome;
+  private puppeteer;
+  private options = {};
   constructor(private config: ConfigService) {}
   async getDataProducstByPuppeteer(
     productSearch: string,
   ): Promise<ScrapperData> {
+    this.setConfiguration();
     let data = null;
-    const browser = await puppeteer.launch({
-      headless: 'new',
-    });
+
+    const browser = await this.puppeteer.launch(this.options);
     const page = await browser.newPage();
     await page.goto(`${this.config.get('URLSCRAPPER')}${productSearch}`);
 
@@ -52,5 +56,26 @@ export class ScrapperService {
 
     await browser.close();
     return data;
+  }
+
+  private async setConfiguration(): Promise<void> {
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION === 'true') {
+      console.log('lalala', process.env.AWS_LAMBDA_FUNCTION_VERSION);
+      this.chrome = require('chrome-aws-lambda');
+      this.puppeteer = require('puppeteer-core');
+      this.options = {
+        args: [
+          ...this.chrome.args,
+          '--hide-scrollbars',
+          '--disable-web-security',
+        ],
+        defaultViewport: this.chrome.defaultViewport,
+        executablePath: await this.chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      };
+    } else {
+      this.puppeteer = require('puppeteer');
+    }
   }
 }
